221d181ca1f1acbdc76374c3c9b7f0bd64a78454
Garfield Freeman <gfreeman@paloaltonetworks.com>
2020-05-15 13:41:24 -0700

    adding errors lib

---
+++ plugins/module_utils/errors.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#  Copyright 2020 Palo Alto Networks, Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+class PrismaCloudError(Exception):
+    def __init__(self, message, errlist=None):
+        super(PrismaCloudError, self).__init__(message)
+        self.errlist = errlist
+
+    def __str__(self):
+        ans = super(PrismaCloudError, self).__str__()
+
+        if errlist:
+            ans += ': {0}'.format(
+                self.errlist[0] if len(self.errlist) == 1 else self.errlist
+            )
+
+        return ans
+
+class AlreadyExistsError(PrismaCloudError):
+    pass
+
+class ObjectNotFoundError(PrismaCloudError):
+    pass
+
+class AuthenticationError(PrismaCloudError):
+    pass
+
+class ResponseNotJson(PrismaCloudError):
+    pass
